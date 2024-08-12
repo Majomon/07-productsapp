51b34c9269b920c560a5e30d22a3f24ce686c64d
Mauri <mauri.monzon91@gmail.com>
2024-08-12 11:39:35 -0300

    Interfaces y petición http

---
+++ src/actions/products/getProductsByPage..ts
@@ -0,0 +1,20 @@
+import {tesloApi} from '../../config/api/tesloApi';
+import {TesloProduct} from '../../infrastructure/interfaces/teslo-products.response';
+import {ProductMapper} from '../../infrastructure/mappers/product.mapper';
+
+export const getProductsByPage = async (page: number, limit: number = 20) => {
+  console.log({page, limit});
+  try {
+    const {data} = await tesloApi.get<TesloProduct[]>(
+      `/products?offset=${page}&limit=${limit}`,
+    );
+    /*     const products = data.map(tesloProduct => ProductMapper.tesloProductToEntity(tesloProduct) */
+    //Es lo mismo que hacer esto
+    const products = data.map(ProductMapper.tesloProductToEntity);
+    console.log(products[0]);
+    return products;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error al realizar la peticion');
+  }
+};
