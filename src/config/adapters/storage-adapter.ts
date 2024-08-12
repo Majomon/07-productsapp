1aaeeb9a1d0a8f1b3efe315c39fcd2018097ae46
Mauri <mauri.monzon91@gmail.com>
2024-08-12 10:01:31 -0300

    Async store

---
+++ src/config/adapters/storage-adapter.ts
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export class StorageAdapter {
+  static async getItem(key: string): Promise<string | null> {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      return value;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  static async setItem(key: string, value: string): Promise<void> {
+    try {
+      await AsyncStorage.setItem(key, value);
+    } catch (error) {
+      throw new Error(`Error al setear el elemento ${key} ${value}`);
+    }
+  }
+
+  static async removeItem(key: string): Promise<void> {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (error) {
+      console.log(error);
+      throw new Error(`No se pudo remover la clave ${key}`);
+    }
+  }
+}
